49f70676f1c5e15236c9b7ce636fc4f7283781a0
cat <marvinarivera@gmail.com>
2022-10-15 17:43:04 -0400

    mobile header fix

--- src/Components/FixedHeader/Header.js
+++ src/Components/FixedHeader/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <div className="w-full h-16"></div>
+                <div className="w-full h-16 bg-gray-700"></div>
             </div>
         )
     }
